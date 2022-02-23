434b6f7ef9de610858bd276fdc8126d9c48ec163
Ye Tian <tianye0718@gmail.com>
2022-02-23 21:50:02 +1300

    0094. Binary tree inorder traversal

---
+++ go/94.二叉树的中序遍历.go
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.60%)
+ * Likes:    1255
+ * Dislikes: 0
+ * Total Accepted:    673.6K
+ * Total Submissions: 890.5K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ *
+ *
+ * 示例 5：
+ *
+ *
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100
+ *
+ *
+ *
+ *
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// Recursive
+// func inorderTraversal(root *TreeNode) []int {
+// 	var res []int
+// 	var traversal func(node *TreeNode)
+// 	traversal = func(node *TreeNode) {
+// 		if node == nil {
+// 			return
+// 		}
+// 		traversal(node.Left)
+// 		res = append(res, node.Val)
+// 		traversal(node.Right)
+// 	}
+// 	traversal(root)
+// 	return res
+// }
+
+// Iterative
+func inorderTraversal(root *TreeNode) (res []int) {
+	var stack []*TreeNode
+	for root != nil || len(stack) != 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+		root = node.Right
+	}
+	return res
+}
+
+// @lc code=end
