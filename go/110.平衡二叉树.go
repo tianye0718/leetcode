88d198bcf99e277085f6460c691626d1d8eb895c
Ye Tian <tianye0718@gmail.com>
2022-02-25 19:45:42 +1300

    0110. Balanced binary tree

---
+++ go/110.平衡二叉树.go
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=golang
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (56.55%)
+ * Likes:    881
+ * Dislikes: 0
+ * Total Accepted:    299.9K
+ * Total Submissions: 529.8K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ *
+ * 本题中，一棵高度平衡二叉树定义为：
+ *
+ *
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：true
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4
+ *
+ *
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	var helper func(root *TreeNode) int
+	helper = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+		left := helper(root.Left)
+		right := helper(root.Right)
+		if left == -1 || right == -1 || absDif(left, right) > 1 {
+			return -1
+		}
+		return max(left, right) + 1
+	}
+	return helper(root) != -1
+}
+func absDif(x, y int) int {
+	if x > y {
+		return x - y
+	}
+	return y - x
+}
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
