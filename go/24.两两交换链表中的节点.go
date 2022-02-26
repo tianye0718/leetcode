b76b2813e1494b5c2df306ce2e1a7f158f96de62
Ye Tian <tianye0718@gmail.com>
2022-02-26 17:15:10 +1300

    0024. Swap Nodes in Pairs

---
+++ go/24.两两交换链表中的节点.go
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=golang
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (70.63%)
+ * Likes:    1248
+ * Dislikes: 0
+ * Total Accepted:    382.4K
+ * Total Submissions: 541.4K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ *
+ *
+ */
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+	curr := dummyHead
+	for curr.Next != nil && curr.Next.Next != nil {
+		firstNode := curr.Next
+		thirdNode := curr.Next.Next.Next
+
+		curr.Next = curr.Next.Next
+		curr.Next.Next = firstNode
+		firstNode.Next = thirdNode
+
+		curr = firstNode
+	}
+	return dummyHead.Next
+}
+
+// @lc code=end
