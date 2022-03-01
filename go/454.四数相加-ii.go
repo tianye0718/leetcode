1000616c2264290f230891c5c610d144c990a810
Ye Tian <tianye0718@gmail.com>
2022-03-02 00:00:49 +1300

    0454. 4 Sum II

---
+++ go/454.四数相加-ii.go
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=454 lang=golang
+ *
+ * [454] 四数相加 II
+ *
+ * https://leetcode-cn.com/problems/4sum-ii/description/
+ *
+ * algorithms
+ * Medium (61.64%)
+ * Likes:    504
+ * Dislikes: 0
+ * Total Accepted:    100K
+ * Total Submissions: 161.9K
+ * Testcase Example:  '[1,2]\n[-2,-1]\n[-1,2]\n[0,2]'
+ *
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l)
+ * 能满足：
+ *
+ *
+ * 0 <= i, j, k, l < n
+ * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+ * 输出：2
+ * 解释：
+ * 两个元组如下：
+ * 1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) +
+ * (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) +
+ * (-1) + 0 = 0
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums1.length
+ * n == nums2.length
+ * n == nums3.length
+ * n == nums4.length
+ * 1 <= n <= 200
+ * -2^28 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2^28
+ *
+ *
+ */
+package main
+
+// @lc code=start
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) (count int) {
+	m := make(map[int]int, 0)
+	for _, a := range nums1 {
+		for _, b := range nums2 {
+			m[a+b]++
+		}
+	}
+	for _, c := range nums3 {
+		for _, d := range nums4 {
+			count += m[-c-d]
+		}
+	}
+	return count
+}
+
+// @lc code=end
