38c8bb0d6e281424da43b4f26a672241500a89bf
Ye Tian <tianye0718@gmail.com>
2022-03-06 20:24:40 +1300

    0098. Validate Binary Search Tree

---
+++ go/98.验证二叉搜索树.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=golang
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (35.53%)
+ * Likes:    1441
+ * Dislikes: 0
+ * Total Accepted:    439.2K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ *
+ *
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [2,1,3]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目范围在[1, 10^4] 内
+ * -2^31 <= Node.val <= 2^31 - 1
+ *
+ *
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	return verifyBST(root, nil, nil)
+}
+func verifyBST(node *TreeNode, min *TreeNode, max *TreeNode) bool {
+	if node == nil {
+		return true
+	}
+	if min != nil && node.Val <= min.Val {
+		return false
+	}
+	if max != nil && node.Val >= max.Val {
+		return false
+	}
+	return verifyBST(node.Left, min, node) && verifyBST(node.Right, node, max)
+
+}
+
+// @lc code=end
