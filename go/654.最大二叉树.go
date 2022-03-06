ed34ba89b1c007aada527cd4f5f23a73826e2951
Ye Tian <tianye0718@gmail.com>
2022-03-06 18:50:21 +1300

    0654. Maximum Binary Tree

---
+++ go/654.最大二叉树.go
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=golang
+ *
+ * [654] 最大二叉树
+ *
+ * https://leetcode-cn.com/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (81.05%)
+ * Likes:    381
+ * Dislikes: 0
+ * Total Accepted:    82K
+ * Total Submissions: 101.2K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+ *
+ *
+ * 创建一个根节点，其值为 nums 中的最大值。
+ * 递归地在最大值 左边 的 子数组前缀上 构建左子树。
+ * 递归地在最大值 右边 的 子数组后缀上 构建右子树。
+ *
+ *
+ * 返回 nums 构建的 最大二叉树 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [3,2,1,6,0,5]
+ * 输出：[6,3,5,null,2,0,null,null,1]
+ * 解释：递归调用如下所示：
+ * - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+ * ⁠   - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+ * ⁠       - 空数组，无子节点。
+ * ⁠       - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+ * ⁠           - 空数组，无子节点。
+ * ⁠           - 只有一个元素，所以子节点是一个值为 1 的节点。
+ * ⁠   - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+ * ⁠       - 只有一个元素，所以子节点是一个值为 0 的节点。
+ * ⁠       - 空数组，无子节点。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[3,null,2,null,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 1000
+ * nums 中的所有整数 互不相同
+ *
+ *
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	return build(nums, 0, len(nums)-1)
+}
+func build(nums []int, start int, end int) *TreeNode {
+	if start > end {
+		return nil
+	}
+	maxVal := nums[start]
+	index := start
+	for i := start + 1; i <= end; i++ {
+		if nums[i] > maxVal {
+			maxVal = nums[i]
+			index = i
+		}
+	}
+	left := build(nums, start, index-1)
+	right := build(nums, index+1, end)
+	root := &TreeNode{
+		Val:   maxVal,
+		Left:  left,
+		Right: right,
+	}
+	return root
+}
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
