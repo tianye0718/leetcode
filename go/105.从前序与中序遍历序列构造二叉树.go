01a82161b2c67c5f8661f50dbc19df4be80e4a6f
Ye Tian <tianye0718@gmail.com>
2022-03-06 18:54:13 +1300

    0105. Construct Binary Tree from Pre and In order Traversal

---
+++ go/105.从前序与中序遍历序列构造二叉树.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=golang
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ *
+ * https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (70.81%)
+ * Likes:    1457
+ * Dislikes: 0
+ * Total Accepted:    308.7K
+ * Total Submissions: 435.8K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder
+ * 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * 输出: [3,9,20,null,null,15,7]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: preorder = [-1], inorder = [-1]
+ * 输出: [-1]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= preorder.length <= 3000
+ * inorder.length == preorder.length
+ * -3000 <= preorder[i], inorder[i] <= 3000
+ * preorder 和 inorder 均 无重复 元素
+ * inorder 均出现在 preorder
+ * preorder 保证 为二叉树的前序遍历序列
+ * inorder 保证 为二叉树的中序遍历序列
+ *
+ *
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := preorder[0]
+	index := 0
+	for i, v := range inorder {
+		if v == root {
+			index = i
+			break
+		}
+	}
+	rootNode := &TreeNode{
+		Val:   root,
+		Left:  buildTree(preorder[1:index+1], inorder[:index]),
+		Right: buildTree(preorder[index+1:], inorder[index+1:]),
+	}
+	return rootNode
+}
+
+// @lc code=end
