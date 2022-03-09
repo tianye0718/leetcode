dcf7a4a77b7cd84f3227e5a6cabbfc456ef870c2
tianye0718 <tianye0718@gmail.com>
2022-03-09 23:54:43 +1300

    2006. Count number of pairs with absolute diff k

---
+++ go/2006.差的绝对值为-k-的数对数目.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=2006 lang=golang
+ *
+ * [2006] 差的绝对值为 K 的数对数目
+ *
+ * https://leetcode-cn.com/problems/count-number-of-pairs-with-absolute-difference-k/description/
+ *
+ * algorithms
+ * Easy (85.74%)
+ * Likes:    68
+ * Dislikes: 0
+ * Total Accepted:    41.6K
+ * Total Submissions: 48.5K
+ * Testcase Example:  '[1,2,2,1]\n1'
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回数对 (i, j) 的数目，满足 i < j 且 |nums[i] - nums[j]| == k
+ * 。
+ *
+ * |x| 的值定义为：
+ *
+ *
+ * 如果 x >= 0 ，那么值为 x 。
+ * 如果 x < 0 ，那么值为 -x 。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,2,1], k = 1
+ * 输出：4
+ * 解释：差的绝对值为 1 的数对为：
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1,3], k = 3
+ * 输出：0
+ * 解释：没有任何数对差的绝对值为 3 。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：nums = [3,2,1,5,4], k = 2
+ * 输出：3
+ * 解释：差的绝对值为 2 的数对为：
+ * - [3,2,1,5,4]
+ * - [3,2,1,5,4]
+ * - [3,2,1,5,4]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 100
+ * 1 <= k <= 99
+ *
+ *
+ */
+package leetcode2006
+
+// @lc code=start
+func countKDifference(nums []int, k int) (res int) {
+	m := make(map[int]int, 0)
+	for _, num := range nums {
+		res += m[num-k] + m[num+k]
+		m[num]++
+	}
+	return res
+}
+
+// @lc code=end
